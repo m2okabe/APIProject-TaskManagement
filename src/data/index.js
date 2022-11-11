7fca379b41287f28906107dd5b151e75654ba60d
Mako Okabe <apricot.mako.0731@gmail.com>
2022-11-11 15:18:46 +0900

    remove unnecessary files

--- src/data/index.js
+++
@@ -1,9 +0,0 @@
-const { pokemon } = require("./pokemon.json");
-const { attacks } = require("./attacks.json");
-const { types } = require("./types.json");
-
-module.exports = {
-  pokemon,
-  attacks,
-  types,
-};
