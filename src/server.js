cb2059b9fa729474506e99ae9930284a34aeca64
Mako Okabe <apricot.mako.0731@gmail.com>
2022-11-12 00:38:16 +0900

    add front getlist insert/update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const taskController = require('./task/task.controller');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const setupServer = () => {
   // express setting
   const app = express();
   app.use(express.json());
   app.use(express.static('web'));
+
+  // needed to receive request body of post api
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   // POST API
   app.post('/api/taskManagement/tasks', taskController.insertOrUpdateTask);
